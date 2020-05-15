9baaf82ac045f2743e0411635b31218077744a7c
Augusto Oazi <hello@augustooazi.com>
2020-05-15 10:45:54 -0300

    first commit

---
+++ .eleventy.js
@@ -0,0 +1,15 @@
+module.exports = function(config) {
+  
+  return {
+    dir: {
+      input: "src",
+      output: "dist",
+      data: "_data"
+    },
+    templateFormats: ["njk", "md", "11ty.js"],
+    htmlTemplateEngine: "njk",
+    markdownTemplateEngine: "njk",
+    passthroughFileCopy: true
+  };
+
+};
